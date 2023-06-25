8807f33d8364f931bc52170af9e73cf0e26ac863
iYzee23 <predrag.pesic17@gmail.com>
2023-06-25 14:48:17 +0200

    Everything working.

--- dummy.py
+++ dummy.py
@@ -4,7 +4,7 @@ import json
 with open("./tests/keys.json", "w") as file:
     json.dump(
         Account.encrypt(
-            private_key="0x219638b363ed5c438bb04e32f5b540eb8acf20c39b72925bf66384c692f5e51f",
+            private_key="0x4657de4f81339a28d3905ef88ae22bc96f726b3c3189e8584be6c1c0238083e6",
             password="iep_project"
         ),
         file
